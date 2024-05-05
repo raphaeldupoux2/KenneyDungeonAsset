f86d776e64d0ae0f52b466fb20028fe8bea34d40
seborok <rapdpx@gmail.com>
2024-05-05 22:04:59 +0200

    first commit

---
+++ CharacterBody2D.gd
@@ -0,0 +1,28 @@
+extends CharacterBody2D
+
+const SPEED = 2
+
+func _physics_process(delta):
+	player_movement(delta)
+
+func player_movement(delta):
+	var velocity = Vector2.ZERO # The player's movement vector.
+	if Input.is_action_pressed(&"move_right"):
+		velocity.x += 1
+	if Input.is_action_pressed(&"move_left"):
+		velocity.x -= 1
+	if Input.is_action_pressed(&"move_down"):
+		velocity.y += 1
+	if Input.is_action_pressed(&"move_up"):
+		velocity.y -= 1
+
+	if velocity.length() > 0:
+		velocity = velocity.normalized() * SPEED
+
+		#$AnimatedSprite2D.play()
+	#else:
+		#$AnimatedSprite2D.stop()
+	move_and_collide(velocity)
+	#position += velocity * delta
+	#position = position.clamp(Vector2.ZERO, screen_size)
+
